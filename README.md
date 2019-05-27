f8ed6defb320db728145b67388a76f2434bae5ad
xfh <m13928810941@163.com>
2019-05-27 11:20:34 +0800

    add

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # php basic learning 2019-05-25
 
-困惑,如何成为高级PHP程序员
+困惑,如何成为高级PHP程序员or全栈程序员or架构师
+
+## 基础巩固
+
+PHP Hypertext Preprocessor
+
+### 字符编码原理
+
+双引号解析变量、转义字符(\t \r \n \\ \ddd八进制 \xhh十六位)
+
+## 项目1 laravel 博客项目
+
+## 项目2 待定
--- index.php
+++ index.php
@@ -1,4 +1,23 @@
 <?php
-
 // phpinfo();
-print_r('hello world');
+// echo "this is a pen , \nthis is a pen." . "\r\n";
+// echo 'this is a pen, \nthis is a pen.' . "\r\n";
+
+// echo "\101\102" . "\r\n";
+// echo "\x41\x42" . "\r\n";
+
+// echo  "\\" . "\r\n";
+// echo  '\\' . "\r\n";
+// htmlspecialchars(); //把预定义的字符转换为HTML实体
+
+/*
+    c语言中字符串结构体
+    struct {
+        char *val;
+        int len;
+    }str;
+    字节组成的数组可以用[]或者{}访问某个字符,字符串长度可以达到2G(内存)
+    常见函数都是单字节处理方式,可以用[]或者{}访问某个字符
+    PHP字符串是二进制安全的
+
+*/
